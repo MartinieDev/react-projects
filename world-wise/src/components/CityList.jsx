57e9505145daf8c78300ddf2ab49df84437bd05a
Matt <matt4real@yahoo.com>
2026-04-17 20:42:38 +0100

    feat(refactor): apply useContext to make app component prop free

--- world-wise/src/components/CityList.jsx
+++ world-wise/src/components/CityList.jsx
@@ -1,16 +1,18 @@
+import { useCities } from '../contexts/CitiesContext';
 import CityItem from './CityItem';
 import styles from './CityList.module.css';
 import Message from './Message';
 import Spinner from './Spinner';
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities()
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
-    
+
   return (
     <div className={styles.cityList}>
       {cities.map((city) => (
---
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:9000/cities';
+
+const CitiesContext = createContext(); // create/initialize a context which returns a context object and is like an identifier
+
+// Context provider component (it's a component cuz it returns jsx)
+function CitiesProvider({ children }) {
+  // Managing state
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Handling logic (effects,updates etc)
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}`);
+        const data = await res.json();
+        setCities(data);
+        return data;
+      } catch (err) {
+        console.log('error:', err);
+        alert('There was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  // Provide value to all the components inside the provider using Context.Provider
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+// custom hook that enables us consume the value. Note we pass context object not the value
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside the CitiesProvider');
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
