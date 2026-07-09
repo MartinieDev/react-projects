b8bc5d37050c46066521a4a9594a6a828cb33e25
Matt <matt4real@yahoo.com>
2026-07-09 18:49:29 +0100

    include redux-intro project

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -10,17 +10,25 @@ import {
 } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
+  const navigate = useNavigate();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
 
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLoacationPosition,
+    getPosition,
+  } = useGeolocation();
 
-  console.log(mapLat, mapLng);
+  const [mapLat, mapLng] = useUrlPosition()
+
+
+  // console.log(mapLat, mapLng, geoLoacationPosition);
 
   useEffect(
     function () {
@@ -31,8 +39,22 @@ function Map() {
     [mapLat, mapLng],
   );
 
+  useEffect(
+    function () {
+      if (geoLoacationPosition)
+        setMapPosition([geoLoacationPosition.lat, geoLoacationPosition.lng]);
+    },
+    [geoLoacationPosition],
+  );
+
   return (
-    <div className={styles.mapContainer} >
+    <div className={styles.mapContainer}>
+      {!geoLoacationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
+
       <MapContainer
         center={mapPosition}
         zoom={6}
@@ -50,6 +72,14 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+
+        {geoLoacationPosition && (
+          <Marker
+            position={[geoLoacationPosition.lat, geoLoacationPosition.lng]}
+          >
+            <Popup>Your current location</Popup>
+          </Marker>
+        )}
 
         <ChangeCenter position={mapPosition} />
         <DetectClicks />
