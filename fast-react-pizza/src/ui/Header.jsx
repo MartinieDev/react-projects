2d7efbf0d96f4dca31d7b458df955654aa8daf80
Matt <matt4real@yahoo.com>
2026-07-18 10:19:32 +0100

    feat: implement the app layout

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Co.</Link>
+
+      <p>Martins</p>
+    </header>
+  )
+}
+
+export default Header
