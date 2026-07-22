641bf3535fca30e3ee5197f65711340ac87846c3
Matt <matt4real@yahoo.com>
2026-07-22 19:05:50 +0100

    feat: configure tailwind

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/Username";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-4 uppercase sm:px-6">
+    <header className="font-sans flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-4 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Co.
       </Link>
--- fast-react-pizza/tailwind.config.js
+++ fast-react-pizza/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        newColor: '#f2b4',
+      },
+
+      fontFamily: {
+        sans: ['monospace', 'Yuyu Short'],
+      },
+
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
-}
+};
