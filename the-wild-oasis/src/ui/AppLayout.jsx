d0ec048bf3f1c3db0d9a8bc64aee91b6d45a6033
Matt <matt4real@yahoo.com>
2026-08-15 15:49:56 +0100

    feat: implement app layout

---
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -0,0 +1,56 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import styled from 'styled-components';
+
+const StyledAppLayout = styled.div`
+  display: grid;
+
+  /* Create two columns:
+     - Sidebar = 26rem
+     - Remaining space = 1fr */
+  grid-template-columns: 26rem 1fr;
+
+  /* Create two rows:
+     - Header gets the height it needs
+     - Main takes the remaining height */
+  grid-template-rows: auto 1fr;
+
+  /* Make the entire layout fill the screen */
+  height: 100vh;
+`;
+
+const Main = styled.main`
+   background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+
+      <Sidebar />
+
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
+
+//               COLUMN 1       COLUMN 2
+//              26rem wide        1fr
+//           ┌──────────────┬──────────────────┐
+// ROW 1     │              │                  │
+// auto      │   Sidebar?   │      Header      │
+//           │              │                  │
+//           ├──────────────┼──────────────────┤
+// ROW 2     │              │                  │
+// 1fr       │              │                  │
+//           │   Sidebar    │       Main       │
+//           │              │                  │
+//           │              │                  │
+//           └──────────────┴──────────────────┘
