fd6a3c44030c1f44214ba5d954ada06578a37427
Matt <matt4real@yahoo.com>
2026-08-19 19:26:45 +0100

    feat: implement toast notifier

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -21,8 +21,16 @@ const StyledAppLayout = styled.div`
 `;
 
 const Main = styled.main`
-   background-color: var(--color-grey-50);
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
@@ -33,7 +41,9 @@ function AppLayout() {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
